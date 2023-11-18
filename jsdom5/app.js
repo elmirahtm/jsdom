bf5e21951a90b3a2b77c084fbfdf8b2753e26856
elmirahtm <elmirahatamova28@gmail.com>
2023-11-18 11:56:05 +0400

    jsdom5

---
+++ jsdom5/app.js
@@ -0,0 +1,15 @@
+
+// 4. Asagidaki funksiyalari yazin:
+// kebabToSnake('ali-greenheart');//ALI_GREENHEART
+// SnakeToKebab('JAVASCRIPT_IS_HELL'); // javascript-is-hell
+let name="ali-greenheart";
+
+const kebabtosnake=()=>{
+    console.log(name.toUpperCase());
+}
+kebabtosnake();
+let ad="JAVASCRIPT-IS-HELL";
+const snaketokebab=()=>{
+    console.log(ad.toLowerCase());
+}
+snaketokebab();
